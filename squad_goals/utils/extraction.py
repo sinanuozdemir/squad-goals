611ea9fc7248d1f4464ec985067ef6a9edde35fe
Sinan Ozdemir <sinan.u.ozdemir@gmail.com>
2025-01-26 09:46:24 -0800

    json parse

--- squad_goals/utils/extraction.py
+++ squad_goals/utils/extraction.py
@@ -4,14 +4,15 @@ import re
 
 def extract_json_from_string(input_string):
     # Use regex to find the JSON part
-    input_string = re.sub(r'[\x00-\x1F\x7F]', '', input_string)  # Remove control characters
+    # input_string = re.sub(r'[\x00-\x1F\x7F]', '', input_string)  # Remove control characters
 
-    match = re.search(r'({.*?})', input_string, re.DOTALL)
+    match = re.search(r'.*?({[^{}]*({[^{}]*})*[^{}]*})', input_string, re.DOTALL)
     if match:
+        print('using json')
         json_str = match.group(1)
         try:
             # Load the JSON string as a dictionary
-            return json.loads(json_str)
+            return json.loads(json_str, strict=False)
         except json.JSONDecodeError:
             return None
     # now use regex to find a dictionary or list that we can eval
